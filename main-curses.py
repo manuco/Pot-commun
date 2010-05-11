9e81c64b2f487ac0ec132944c051eab478579c2d
Emmanuel <manuco@club-internet.fr>
2010-05-11 15:43:01 +0200

    Set up curses display, color, sizing, etc

--- main-curses.py
+++ main-curses.py
@@ -3,8 +3,11 @@
 from __future__ import  division
 
 import curses
+import curses as c
 import curses.wrapper
 import potcommun
+
+
 
 ## Tip : export ESCDELAY var to reduce time to interpret escape key
 
@@ -18,7 +21,8 @@ class BaseForm(object):
     def onInput(self, ch, key):
         """
             Called when an input has been received
-            @return the new form to display (self is right), or None to quit
+            @return the new form to display (self is right), or None to quit, or
+            an int corresponding to the forms to unstack (usualy 1).
         """
         # noop
         return self
@@ -30,60 +34,103 @@ class WelcomeForm(BaseForm):
         my, mx = win.getmaxyx()
         x = mx // 2 - len(title) // 2
         y = my // 2
-        win.addstr(y, x, title, curses.COLOR_YELLOW)
+        win.addstr(y, x, title, YELLOW | c.A_BOLD)
 
     def onInput(self, ch, key):
-        return self
+        if ch == 27 or key in ('q', 'Q'):
+            return 1
+        elif ch == 13:
+            return self
+        else:
+            return self
 
 class PotCommunCursesApplication(object):
 
     def __init__(self, mainWindow):
         self.form = WelcomeForm()
+        self.formStack = []
         self.mainWindow = mainWindow
-        self.workspace = self.createTitle()
+        self.workspace = self.createWorkspace()
         self.mainWindow.notimeout(True)
         self.workspace.notimeout(True)
         self.workspace.keypad(True)
         self.lastKey = "None"
 
-    def createTitle(self):
+    def createWorkspace(self):
         return self.mainWindow.subwin(2, 0)
 
-    def draw(self):
+    def draw(self, resized):
         self.mainWindow.erase()
         title = "Pot Commun V %s" % potcommun.__version__
         my, mx = self.mainWindow.getmaxyx()
         x = mx // 2 - len(title) // 2
-        self.mainWindow.addstr(0, x, title, curses.COLOR_WHITE | curses.A_BOLD)
+        self.mainWindow.addstr(0, x, title, WHITE | c.A_BOLD)
         for n in range(mx):
-            self.mainWindow.addch(1, n, curses.ACS_HLINE)
+            self.mainWindow.addch(1, n, curses.ACS_HLINE, WHITE)
         my, mx = self.mainWindow.getmaxyx()
-        self.mainWindow.addstr(0, mx - 1 - len(self.lastKey), self.lastKey, curses.COLOR_GREEN)
+        self.mainWindow.addstr(0, mx - 1 - len(self.lastKey), self.lastKey, CYAN)
+        if resized:
+            del self.workspace
+            self.workspace = self.createWorkspace()
         self.form.draw(self.workspace)
 
         self.mainWindow.noutrefresh()
         self.workspace.noutrefresh()
 
+    def formatKey(self, ch, key):
+        fd = open("/tmp/log", "a")
+        if key is None:
+            r = "None - XXX - XXX"
+        elif ch == 13:
+            r = "RETURN - " + str(ch) + " - '" + curses.unctrl(ch) + "'"
+        else:
+            r = key + " - " + str(ch) + " - '" + curses.unctrl(ch) + "'"
+        fd.write(r + "\n")
+        fd.close()
+
+        return r
+
+
+
     def main(self):
+        ch = 0
         while self.form:
+            try:
+                self.draw(ch == curses.KEY_RESIZE)
+            except Exception, e:
+                self.mainWindow.addstr(0, 0, "EE")
+
+            curses.doupdate()
             ch, key = self.prompt()
+            self.lastKey = self.formatKey(ch, key)
             if key is None:
                 return 1
-            elif ch == 13:
-                self.lastKey = "RETURN - " + str(ch) + " - '" + curses.unctrl(ch) + "'"
-            else:
-                self.lastKey = key + " - " + str(ch) + " - '" + curses.unctrl(ch) + "'"
-            self.form.onInput(ch, key)
+            newForm = self.form.onInput(ch, key)
+            if self.form is not newForm:
+                if newForm is None:
+                    return 0
+                if type(newForm) == type(1):
+                    try:
+                        for i in range(newForm):
+                            self.form = self.formStack.pop()
+                    except IndexError:
+                        return 0
+                else:
+                    self.formStack.append(self.form)
+                    self.form = newForm
 
     def prompt(self):
         ## See http://bugs.python.org/issue1687125
         try:
             try:
-                self.draw()
-                curses.doupdate()
                 ch = self.mainWindow.getch()
+                if ch == -1:
+                    ch = self.mainWindow.getch()
                 curses.ungetch(ch)
-                key = self.mainWindow.getkey()
+                try:
+                    key = self.mainWindow.getkey()
+                except Exception, e:
+                    key = str(e.args)
                 return ch, key
             except KeyboardInterrupt:
                 return None, None
@@ -93,6 +140,23 @@ class PotCommunCursesApplication(object):
 
 def main(mainWindow):
     curses.nonl()
+    curses.use_default_colors()
+
+    curses.init_pair(1, curses.COLOR_RED, -1)
+    curses.init_pair(2, curses.COLOR_GREEN, -1)
+    curses.init_pair(3, curses.COLOR_BLUE, -1)
+    curses.init_pair(4, curses.COLOR_YELLOW, -1)
+    curses.init_pair(5, curses.COLOR_MAGENTA, -1)
+    curses.init_pair(6, curses.COLOR_CYAN, -1)
+    curses.init_pair(7, curses.COLOR_WHITE, -1)
+    global RED, GREEN, BLUE, YELLOW, MAGENTA, CYAN, WHITE
+    RED = curses.color_pair(1)
+    GREEN = curses.color_pair(2)
+    BLUE = curses.color_pair(3)
+    YELLOW = curses.color_pair(4)
+    MAGENTA = curses.color_pair(5)
+    CYAN = curses.color_pair(6)
+    WHITE = curses.color_pair(7)
     app = PotCommunCursesApplication(mainWindow)
     app.main()
 
