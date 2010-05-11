ba1ff8e402bff6baa0716695ba2d49fed284c08e
Emmanuel <manuco@club-internet.fr>
2010-05-11 18:30:31 +0200

    Work in progress
     * fix key handling when term is resized
     * fix color handling

--- main-curses.py
+++ main-curses.py
@@ -6,10 +6,12 @@ import curses
 import curses as c
 import curses.wrapper
 import potcommun
+import traceback
 
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
-
-## Tip : export ESCDELAY var to reduce time to interpret escape key
+## Tip : export ESCDELAY var to reduce time to interpret escape key (10 is fine)
 
 class BaseForm(object):
     def draw(self, win):
@@ -27,6 +29,49 @@ class BaseForm(object):
         # noop
         return self
 
+    def drawBox(self, win, y, x, height, width, color):
+        """
+            Draw a box.
+
+            Height must be a least 3 to have one line inside it. Same for width.
+        """
+        ## corners
+        #win.addstr(0,0,str(width))
+
+        my, mx = win.getmaxyx()
+        if mx < x + width or my < y + height or x < 0 or y < 0:
+            return
+
+        win.addch(y, x, c.ACS_ULCORNER , WHITE)
+        win.addch(y, x + width - 1, c.ACS_URCORNER , WHITE)
+        win.addch(y + height - 1, x + width - 1, c.ACS_LRCORNER , WHITE)
+        win.addch(y + height - 1, x, c.ACS_LLCORNER , WHITE)
+        for n in range(width - 2):
+            win.addch(y, x + n + 1, c.ACS_HLINE, WHITE)
+            #win.addstr(y, x + n + 1, str(n)[-1], WHITE)
+            win.addch(y + height - 1, x + n + 1, c.ACS_HLINE, WHITE)
+
+        for n in range(height - 2):
+            win.addch(y + n + 1, x, c.ACS_VLINE, WHITE)
+            win.addch(y + n + 1, x + width - 1, c.ACS_VLINE, WHITE)
+
+        #for i in range(60):
+            #win.addstr(2, i, str(i)[-1], WHITE)
+        #win.addstr(1,0,str(x + width - 1))
+
+
+    def drawTitle(self, win, title, color):
+        my, mx = win.getmaxyx()
+        minWidth = len(title) + 3
+        maxX = mx // 2 - minWidth // 2 - 1
+        x = mx // 4 if mx // 4 < maxX else maxX
+        width = mx // 2 if mx // 2 > minWidth else minWidth
+        y = 0
+        height = 3
+        self.drawBox(win, y, x, height, width, WHITE)
+        x = mx // 2 - len(title) // 2
+        win.addstr(1, x, title, WHITE)
+
 class WelcomeForm(BaseForm):
     def draw(self, win):
         BaseForm.draw(self, win)
@@ -40,9 +85,25 @@ class WelcomeForm(BaseForm):
         if ch == 27 or key in ('q', 'Q'):
             return 1
         elif ch == 13:
+            return DebtManagerSelection()
+        else:
+            return self
+
+
+class DebtManagerSelection(BaseForm):
+    def draw(self, win):
+        BaseForm.draw(self, win)
+        self.drawTitle(win, "Sélection du pot commun", WHITE)
+
+    def onInput(self, ch, key):
+        if ch == 27 or key in ('q', 'Q'):
+            return 1
+        elif ch == 13:
             return self
         else:
             return self
+
+
 
 class PotCommunCursesApplication(object):
 
@@ -59,16 +120,19 @@ class PotCommunCursesApplication(object):
     def createWorkspace(self):
         return self.mainWindow.subwin(2, 0)
 
+    def drawAppTitle(self, win, title):
+        my, mx = win.getmaxyx()
+        x = mx // 2 - len(title) // 2
+        win.addstr(0, x, title, WHITE | c.A_BOLD)
+        for n in range(mx):
+            win.addch(1, n, curses.ACS_HLINE, CYAN)
+        my, mx = win.getmaxyx()
+        win.addstr(0, mx - 1 - len(self.lastKey), self.lastKey, GREEN)
+
     def draw(self, resized):
         self.mainWindow.erase()
-        title = "Pot Commun V %s" % potcommun.__version__
-        my, mx = self.mainWindow.getmaxyx()
-        x = mx // 2 - len(title) // 2
-        self.mainWindow.addstr(0, x, title, WHITE | c.A_BOLD)
-        for n in range(mx):
-            self.mainWindow.addch(1, n, curses.ACS_HLINE, WHITE)
-        my, mx = self.mainWindow.getmaxyx()
-        self.mainWindow.addstr(0, mx - 1 - len(self.lastKey), self.lastKey, CYAN)
+        self.drawAppTitle(self.mainWindow, "Pot Commun V %s" % potcommun.__version__)
+
         if resized:
             del self.workspace
             self.workspace = self.createWorkspace()
@@ -78,16 +142,16 @@ class PotCommunCursesApplication(object):
         self.workspace.noutrefresh()
 
     def formatKey(self, ch, key):
-        fd = open("/tmp/log", "a")
+
         if key is None:
             r = "None - XXX - XXX"
         elif ch == 13:
             r = "RETURN - " + str(ch) + " - '" + curses.unctrl(ch) + "'"
         else:
             r = key + " - " + str(ch) + " - '" + curses.unctrl(ch) + "'"
-        fd.write(r + "\n")
+        fd = open("/tmp/log", "a")
+        fd.write("-> %s" % r)
         fd.close()
-
         return r
 
 
@@ -98,7 +162,12 @@ class PotCommunCursesApplication(object):
             try:
                 self.draw(ch == curses.KEY_RESIZE)
             except Exception, e:
-                self.mainWindow.addstr(0, 0, "EE")
+                try:
+                    self.mainWindow.addstr(0, 0, "EE " + e.args[0], RED | c.A_BOLD)
+                    self.mainWindow.addstr(2, 0, traceback.format_exc(), WHITE | c.A_BOLD)
+
+                except:
+                    self.mainWindow.addstr(0, 0, "EE", RED | c.A_BOLD)
 
             curses.doupdate()
             ch, key = self.prompt()
@@ -123,14 +192,26 @@ class PotCommunCursesApplication(object):
         ## See http://bugs.python.org/issue1687125
         try:
             try:
-                ch = self.mainWindow.getch()
-                if ch == -1:
+                fd = open("/tmp/log", "a")
+
+                ch = -1
+                while ch == -1:
                     ch = self.mainWindow.getch()
-                curses.ungetch(ch)
-                try:
-                    key = self.mainWindow.getkey()
-                except Exception, e:
-                    key = str(e.args)
+                    fd.write("ch -> %d\n" % (ch))
+                    if ch == -1:
+                        curses.doupdate()
+                        fd.write("doupdate\n")
+                        continue
+                    curses.ungetch(ch)
+                    try:
+                        key = self.mainWindow.getkey()
+                    except Exception, e:
+                        key = str(e.args)
+                    fd.write("key -> %s\n" % key)
+
+                fd.write("\n")
+                fd.close()
+
                 return ch, key
             except KeyboardInterrupt:
                 return None, None
